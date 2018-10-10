59e4fc0966ac2cff6844e4876995e4f09deb2cc0
Haisam K. Ido <haisam.ido@nasa.gov>
2018-10-10 17:47:25 -0400

    Updated configuration script for mac os x

--- configMacOSX.sh
+++ configMacOSX.sh
@@ -12,10 +12,14 @@ brew install cask
 brew update && brew cleanup
 
 # Installs shell applications
-brew install jq bash tree tmux wget lz4 wget curl rsync octave gnuplot 
+brew install jq bash tree tmux wget lz4 wget curl rsync octave gnuplot ansible openvpn speedtest_cli 
 
 # Install gnu dev tools
-gawk gcc gnu-sed coreutils colordiff speedtest_cli autoconf go graphviz poppler consul
+brew install make autoconf gawk gcc gnu-sed coreutils colordiff 
+
+# Installs more goodies
+brew install go graphviz poppler consul
+pip install Flask
 
 # Installs aws cli and shell tools
 brew install aws-shell awscli
@@ -24,10 +28,9 @@ brew install aws-shell awscli
 brew cask install virtualbox
 brew cask install java
 brew cask install google-chrome firefox
-brew cask install postgres pgadmin4 
+brew cask install postgres pgadmin4
 brew cask install visual-studio-code sublime-text webstorm google-web-designer pycharm-ce atom emacs jedit 
 brew cask install Slack iTerm2 gimp inkscape Cyberduck xoctave scilab 
 brew cask install meld DiffMerge
 brew cask install Docker
 brew cask install libreoffice 
-
