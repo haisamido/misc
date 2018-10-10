8b4a4b64996412ba957819562b49e82829d4a1d4
Haisam K. Ido <haisam.ido@nasa.gov>
2018-10-10 18:14:54 -0400

    Updates

--- configMacOSX.sh
+++ configMacOSX.sh
@@ -12,17 +12,19 @@ brew install cask
 brew update && brew cleanup
 
 # Installs shell applications
-brew install jq bash tree tmux wget lz4 wget curl rsync octave gnuplot ansible openvpn speedtest_cli 
+brew install bash bash-completion jq tree tmux wget lz4 wget curl rsync octave gnuplot ansible openvpn speedtest_cli gnupg
 
 # Install gnu dev tools
-brew install make autoconf gawk gcc gnu-sed coreutils colordiff 
+brew install git make autoconf gawk gcc gnu-sed coreutils colordiff 
 
 # Installs more goodies
-brew install go graphviz poppler consul
+brew install go graphviz poppler consul mysql
 pip install Flask
 
 # Installs aws cli and shell tools
 brew install aws-shell awscli
+curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-darwin-amd64-latest && chmod +x /usr/local/bin/ecs-cli
+# cd ~/Downloads && wget https://s3.amazonaws.com/publicsctdownload/MacOS/aws-schema-conversion-tool-1.0.latest.zip && unzip aws-schema-conversion-tool-1.0.latest.zip 
 
 # Installs native mac os x apps
 brew cask install virtualbox
@@ -33,4 +35,6 @@ brew cask install visual-studio-code sublime-text webstorm google-web-designer p
 brew cask install Slack iTerm2 gimp inkscape Cyberduck xoctave scilab 
 brew cask install meld DiffMerge
 brew cask install Docker
-brew cask install libreoffice 
+brew cask install libreoffice
+#brew cask install mamp
+
